9da182293f2c1b33b98a54298f9c04d9383c9671
Natalia Francisca <nathifrancisca@gmail.com>
2022-03-02 20:28:36 -0300

    add database

---
+++ src/db.js
@@ -0,0 +1,104 @@
+export const data_songs = [
+    {
+        "name": "Tasha & Tracie",
+        "spotify_link": "https://open.spotify.com/artist/5Gv1C1LY8pWiYcfcdjSNMT?si=BngPzWshT1irP5QNfuIojw",
+        "container-name": "tasha-tracie",
+    },
+
+    {
+        "name": "Baco Exu do Blues",
+        "spotify_link": "https://open.spotify.com/artist/78nr1pVnDR7qZH6QbVMYZf?si=jKsb7UPJSESqiUaLdpb-oA",
+        "container-name": "baco-exu-blues",
+    },
+
+    {
+        "name": "Mc Tha",
+        "spotify_link": "https://open.spotify.com/artist/0fpg4Y7Q6ZRDSppLLt7G0n?si=veFYTMBLRyGJ_G0wS4MBfw",
+        "container-name": "mc-tha",
+    },
+
+    {
+        "name": "Gloria Groove",
+        "spotify_link": "https://open.spotify.com/artist/7rXMvXRnWHaSwnVvPeUUfw?si=UbNGwnvoQtCUIMxX0_EHcg",
+        "container-name": "gloria-groove",
+    },
+
+    {
+        "name": "Mateus Aleluia",
+        "spotify_link": "https://open.spotify.com/artist/5a4ZtVIIv7tXhpSniKoUdv?si=ttP2BDVCSleW8qIKwR9Ejg",
+        "container-name": "mateus-aleluia",
+    },
+
+    {
+        "name": "Baiana System",
+        "spotify_link": "https://open.spotify.com/artist/5JHYuwE2n7bleXMUsmtCW5?si=LjMu1SbBTCWc37bwYgWC2Q",
+        "container-name": "baiana-system",
+    },
+
+    {
+        "name": "Racionais MC's",
+        "spotify_link": "https://open.spotify.com/artist/29CQLw9uLWsl8Qkz9holfr?si=9JMxx1R1RU64FfurFUjDQQ",
+        "container-name": "racionais",
+    },
+
+    {
+        "name": "Charlie Brown Jr.",
+        "spotify_link": "https://open.spotify.com/artist/1on7ZQ2pvgeQF4vmIA09x5?si=F2YlnwcfS6aMy604OreIew",
+        "container-name": "charlie-brown-jr",
+    },
+
+    {
+        "name": "Pitty",
+        "spotify_link": "https://open.spotify.com/artist/2dmQ0vMD3THLMcz7DsvfaT?si=6zvkhP7JQligQFc8tPYdGw",
+        "container-name": "pitty",
+    },
+
+    {
+        "name": "O Terno",
+        "spotify_link": "https://open.spotify.com/artist/2TuVzZPwEPqpstqmA0qLl6?si=fxDGMWF9Tc6MUdw6q5jJvg",
+        "container-name": "o-terno",
+    },
+
+    {
+        "name": "CPM 22",
+        "spotify_link": "https://open.spotify.com/artist/2Jw4Lrfjnyv2QsDoBgnrAP?si=V4WxJwU-QBWHb_J8owU42A",
+        "container-name": "cpm-22",
+    },
+
+    {
+        "name": "Pabllo Vittar",
+        "spotify_link": "https://open.spotify.com/artist/6tzRZ39aZlNqlUzQlkuhDV?si=kwY83cKHQDyIAgdtTJnppw",
+        "container-name": "pabllo-vittar",
+    },
+
+    {
+        "name": "Juliano Holanda",
+        "spotify_link": "https://open.spotify.com/artist/3wg2dojocgkzfZFx66oHSt?si=BLuK2yNLTFqPsyvJe8agiQ",
+        "container-name": "juliano-holanda",
+    },
+
+    {
+        "name": "Nelson Gonçalves",
+        "spotify_link": "https://open.spotify.com/artist/3wg2dojocgkzfZFx66oHSt?si=BLuK2yNLTFqPsyvJe8agiQ",
+        "container-name": "nelson-gonçalves",
+    },
+
+    {
+        "name": "Iza",
+        "spotify_link": "https://open.spotify.com/artist/3zgnrYIltMkgeejmvMCnes?si=XAAc7peGTQ2uAWvg8gLtHA",
+        "container-name": "iza",
+    },
+
+    {
+        "name": "Tim Maia",
+        "spotify_link": "https://open.spotify.com/artist/0jOs0wnXCu1bGGP7kh5uIu?si=A-jWMgl0QE-CNWttmeeQUA",
+        "container-name": "tim-maia",
+    },
+
+
+
+
+
+
+
+]
