f3f86ca661c375f304190291822997150265f807
Natalia Francisca dos Santos Gomes <natalia.francisca@ibm.com>
2022-03-08 20:57:22 -0300

    update de db

--- src/db.js
+++ src/db.js
@@ -6,8 +6,8 @@ export const data = [
         "text": "Aos 26 anos, as irmãs gêmeas Tasha e Tracie Okereke estão despontando no rap nacional e deixando sua marca na cena.  As cantoras são as responsáveis pela criação do movimento “Expensive Shit” – nome homônimo ao blog que mantém – que visa a valorização da autoestima e da autonomia dos jovens negros, que vivem nas periferias, por meio de conhecimento, arte, moda e informação.",
         "socialMedia": {
             "instagram": [
-                {"name": "Tasha Okereke", "user": "https://www.instagram.com/tashaokereke/?hl=en"}, 
-                {"name": "Tracie Okereke", "user": "https://www.instagram.com/tracieokereke/?hl=en"}
+                {"name": "Tasha Okereke", "user": "https://www.instagram.com/tashaokereke/"}, 
+                {"name": "Tracie Okereke", "user": "https://www.instagram.com/tracieokereke/"}
             ],
             "youtube": "https://www.youtube.com/channel/UCaqFH2lGTICD9iWi8NCdbBw",
             "spotify": "https://open.spotify.com/artist/5Gv1C1LY8pWiYcfcdjSNMT?si=BngPzWshT1irP5QNfuIojw"
@@ -21,7 +21,7 @@ export const data = [
         "image": "images/baco-exu-blues.jpg",
         "text": "Diogo Moncorvo, real nome de Baco Exu do Blues, começou a ganhar destaque pela faixa ‘Suícido’ em parceria com o rapper Diomedes Chinaski e no ano seguinte, em 2017, foi premiado como o artista revelação pelo ‘Prêmio Multishow de Música Brasileira’ e recentemente lançou seu novo álbum ‘Quantas vezes você já foi amado’.",
         "socialMedia": {
-            "instagram": [{"name": "Baco Exu do Blues", "user": "https://www.instagram.com/accounts/login/?next=/exudoblues/"}],
+            "instagram": [{"name": "Baco Exu do Blues", "user": "https://www.instagram.com/exudoblues/"}],
             "youtube": "https://www.youtube.com/channel/UC__z2DsFoNDdWx1vRne7PSg",
             "spotify": "https://open.spotify.com/artist/78nr1pVnDR7qZH6QbVMYZf?si=jKsb7UPJSESqiUaLdpb-oA"
         },
@@ -30,86 +30,186 @@ export const data = [
 
     {
         "name": "Mc Tha",
-        "spotify_link": "https://open.spotify.com/artist/0fpg4Y7Q6ZRDSppLLt7G0n?si=veFYTMBLRyGJ_G0wS4MBfw",
         "container-name": "mc-tha",
+        "image": "images/mc-tha.jpg",
+        "text": "A cantora e compositora paulistana tem 29 anos e nasceu na Cidade Tiradentes, bairro periférico da Zona Leste da capital paulista. Com um álbum e um EP na discografia, MC Tha dialoga entre a MPB e o funk em suas canções e lota suas apresentações por todo o Brasil. Rito de Passá é a música que dá nome ao único álbum lançado pela cantora, em 2019. ",
+        "socialMedia": {
+            "instagram": [{"name": "Mc Tha", "user": "https://www.instagram.com/mcthaa/"}],
+            "youtube": "https://www.youtube.com/channel/UCR3q5aulJYUmBaz0mTiOwrw",
+            "spotify": "https://open.spotify.com/artist/0fpg4Y7Q6ZRDSppLLt7G0n?si=veFYTMBLRyGJ_G0wS4MBfw"
+        },
+        "referencias": ["https://revistamarieclaire.globo.com/Cultura/noticia/2022/03/quem-sao-mulheres-do-lollapalooza-brasil-2022.html"]
     },
 
     {
         "name": "Gloria Groove",
-        "spotify_link": "https://open.spotify.com/artist/7rXMvXRnWHaSwnVvPeUUfw?si=UbNGwnvoQtCUIMxX0_EHcg",
         "container-name": "gloria-groove",
+        "image": "images/gloria-groove.jpg",
+        "text": "Gloria Groove é cantora, dubladora e drag queen brasileira. Ainda com o nome de Daniel Garcia, começou a trabalhar na infância, e fez parte de uma das formações do 'Balão Mágico' em 2002. Em 2006, participou do quadro 'Jovens Talentos' do 'Programa Raul Gil'. Como dubladora, deu voz a diversos desenhos famosos, como 'Hannah Montana' e 'Digimon'. Como cantora de pop e funk, lançou diversos hits, e vem fazendo muito sucesso com seu último lançamento o album Lady Lest",
+        "socialMedia": {
+            "instagram": [{"name": "Gloria Groove", "user": "https://www.instagram.com/gloriagroove/"}],
+            "youtube": "https://www.youtube.com/channel/UCLoL96fSeaLOen3mpDYeqtA",
+            "spotify": "https://open.spotify.com/artist/7rXMvXRnWHaSwnVvPeUUfw?si=UbNGwnvoQtCUIMxX0_EHcg"
+        },
+        "referencias": ["https://revistaquem.globo.com/famoso/gloria-groove/"]
     },
 
     {
         "name": "Mateus Aleluia",
-        "spotify_link": "https://open.spotify.com/artist/5a4ZtVIIv7tXhpSniKoUdv?si=ttP2BDVCSleW8qIKwR9Ejg",
         "container-name": "mateus-aleluia",
+        "image": "images/mateus-aleluia.jpg",
+        "text": "Filho e fruto de Cachoeira, Mateus Aleluia Lima é cantor, compositor e pesquisador da ancestralidade musical pan-africana do Brasil. Iniciou sua carreira em sua cidade natal no Recôncavo Baiano e junto com Dadinho foi responsável pelo perfil artístico ideológico dos Tincoãs, considerado o primeiro grupo vocal a expressar, na história da Música Popular Brasileira, a herança cultural – musical e linguística – de diferentes povos africanos que aqui aportaram.",
+        "socialMedia": {
+            "instagram": [{"name": "Mateus Aleluia", "user": "https://www.instagram.com/mateusaleluia/"}],
+            "youtube": "https://www.youtube.com/channel/UCNpUZ4foOfIrLVbwAbd1SCQ",
+            "spotify": "https://open.spotify.com/artist/5a4ZtVIIv7tXhpSniKoUdv?si=ttP2BDVCSleW8qIKwR9Ejg"
+        },
+        "referencias": ["https://mateusaleluia.com.br/"]
     },
 
     {
         "name": "Baiana System",
-        "spotify_link": "https://open.spotify.com/artist/5JHYuwE2n7bleXMUsmtCW5?si=LjMu1SbBTCWc37bwYgWC2Q",
         "container-name": "baiana-system",
+        "image": "images/baiana-system.jpg",
+        "text": "O BaianaSystem é um projeto musical formado em 2009 com o objetivo de encontrar novas possibilidades sonoras para a guitarra baiana, instrumento criado em Salvador – Bahia nos anos 1940 e que foi responsável pela criação do trio elétrico. O nome vem da junção de “guitarra Baiana” com “sound system“, que são sistemas de som criados e popularizados na Jamaica.",
+        "socialMedia": {
+            "instagram": [{"name": "BaianaSystem", "user": "https://www.instagram.com/baianasystem/"}],
+            "youtube": "https://www.youtube.com/channel/UCiRYHu6rOl1klmJQwKmdgzA",
+            "spotify": "https://open.spotify.com/artist/5JHYuwE2n7bleXMUsmtCW5?si=LjMu1SbBTCWc37bwYgWC2Q"
+        },
+        "referencias": ["https://baianasystem.com.br/bio/"]
     },
 
     {
         "name": "Racionais MC's",
-        "spotify_link": "https://open.spotify.com/artist/29CQLw9uLWsl8Qkz9holfr?si=9JMxx1R1RU64FfurFUjDQQ",
         "container-name": "racionais",
+        "image": "images/racionais.jpg",
+        "text": "O Racionais Mc's é um grupo brasileiro de RAP que surgiu no final dos anos 80 com um discurso que tinha a preocupação de denunciar o racismo e o sistema capitalista opressor que patrocinava a miséria que estava automaticamente ligada com a violência e o crime. Trinta anos depois, Racionais Mc's, ainda com um forte engajamento na luta contra o racismo e discriminação, vem deixando seu legado e construiu uma história ao lado das pessoas que sempre os acompanharam.",  
+        "socialMedia": {
+            "instagram": [{"name": "Racionais", "user": "https://www.instagram.com/racionaiscn/"}],
+            "youtube": "https://www.youtube.com/channel/UCiRYHu6rOl1klmJQwKmdgzA",
+            "spotify": "https://open.spotify.com/artist/29CQLw9uLWsl8Qkz9holfr?si=9JMxx1R1RU64FfurFUjDQQ"
+        },
+        "referencias": ["https://www.racionaisoficial.com.br/"]
     },
 
     {
         "name": "Charlie Brown Jr.",
-        "spotify_link": "https://open.spotify.com/artist/1on7ZQ2pvgeQF4vmIA09x5?si=F2YlnwcfS6aMy604OreIew",
         "container-name": "charlie-brown-jr",
+        "image": "images/charlie-brown-jr.png",
+        "text": "Charlie Brown Jr. foi uma banda brasileira formada em Santos no ano de 1992. Mistura vários ritmos como o hardcore, o reggae, o rap, o skate punk, criando um estilo próprio. As letras de suas músicas fazem críticas à sociedade da perspectiva do universo jovem contemporâneo.",
+        "socialMedia": {
+            "instagram": [{"name": "Charlie Brown Jr.", "user": "https://www.instagram.com/charliebrownjr/"}],
+            "youtube": "https://www.youtube.com/channel/UC5k4fsA9AhxNLgMQa36CVdg",
+            "spotify": "https://open.spotify.com/artist/1on7ZQ2pvgeQF4vmIA09x5?si=F2YlnwcfS6aMy604OreIew"
+        },
+        "referencias": ["https://minutoligado.com.br/bem-estar/charlie-brown-jr-fotos-e-biografia/"]
     },
 
     {
         "name": "Pitty",
-        "spotify_link": "https://open.spotify.com/artist/2dmQ0vMD3THLMcz7DsvfaT?si=6zvkhP7JQligQFc8tPYdGw",
         "container-name": "pitty",
+        "image": "images/pitty.jpg",
+        "text": "Com mais de 20 anos de carreira, Pitty é uma das grandes roqueiras do Brasil. Cantora, compositora, produtora e apresentadora, a inquieta e plural artista utiliza múltiplas plataformas para a produção e a divulgação de seu trabalho. No espaço conquistado, também se posiciona em diversas causas, mantendo-se sempre bem informada e promovendo a troca de ideias.",
+        "socialMedia": {
+            "instagram": [{"name": "Pitty", "user": "https://www.instagram.com/pitty/"}],
+            "youtube": "https://www.youtube.com/channel/UCsemfIhN2JJkYrGiva2lPMw",
+            "spotify": "https://open.spotify.com/artist/2dmQ0vMD3THLMcz7DsvfaT?si=6zvkhP7JQligQFc8tPYdGw"
+        },
+        "referencias": ["https://www.pitty.com.br/bio"]
     },
 
     {
         "name": "O Terno",
-        "spotify_link": "https://open.spotify.com/artist/2TuVzZPwEPqpstqmA0qLl6?si=fxDGMWF9Tc6MUdw6q5jJvg",
         "container-name": "o-terno",
+        "image": "images/o-terno.jpg",
+        "text": "O Terno é um power-trio de canção-rocknroll-pop-experimental de São Paulo-SP formado por Tim Bernardes (Guitarra e Voz), Guilherme d'Almeida (Baixo) e Biel Basile (Bateria). O grupo chama atenção por sua estética autoral, mesclando com originalidade influências e timbres sessentistas e contemporâneos que dão cor às interessantes composições da banda.",
+        "socialMedia": {
+            "instagram": [{"name": "Pitty", "user": "https://www.instagram.com/o_terno/"}],
+            "youtube": "https://www.youtube.com/channel/UCtnJIUHQ9jucYJRaVL3qz2g",
+            "spotify": "https://open.spotify.com/artist/2TuVzZPwEPqpstqmA0qLl6?si=fxDGMWF9Tc6MUdw6q5jJvg"
+        },
+        "referencias": ["https://www.vagalume.com.br/o-terno/biografia/"]
     },
 
     {
         "name": "CPM 22",
-        "spotify_link": "https://open.spotify.com/artist/2Jw4Lrfjnyv2QsDoBgnrAP?si=V4WxJwU-QBWHb_J8owU42A",
         "container-name": "cpm-22",
+        "image": "images/cpm-22.jpg",
+        "text": "CPM 22 é uma banda de hardcore melódico formada nos anos 90.A banda coleciona hits que a colocaram como uma das principais bandas brasileiras de rock da atualidade. Entre as músicas de mais sucesso da banda merecem destaque Regina Let's Go, Um Minuto Para o Fim Do Mundo, Tarde de Outubro, Dias Atrás, Não Sei Viver Sem Ter Você, Irreversível, entre outras.",
+        "socialMedia": {
+            "instagram": [{"name": "CPM 22", "user": "https://www.instagram.com/cpm22/"}],
+            "youtube": "https://www.youtube.com/channel/UCESSgdazM9Sssl3TTLQUAIw",
+            "spotify": "https://open.spotify.com/artist/2Jw4Lrfjnyv2QsDoBgnrAP?si=V4WxJwU-QBWHb_J8owU42A"
+        },
+        "referencias": ["https://www.vagalume.com.br/cpm-22/biografia/"]
     },
 
     {
         "name": "Pabllo Vittar",
-        "spotify_link": "https://open.spotify.com/artist/6tzRZ39aZlNqlUzQlkuhDV?si=kwY83cKHQDyIAgdtTJnppw",
         "container-name": "pabllo-vittar",
+        "image": "images/pabllo-vittar.jpg",
+        "text": "Pabllo Vittar é uma cantora e drag queen brasileira. Nascida em São Luís, no Maranhão, em 1 de novembro de 1993, seu sucesso sem precedentes enquanto artista musical drag queen vem trazendo muita visibilidade para outras artistas drag.",
+        "socialMedia": {
+            "instagram": [{"name": "Pabllo Vittar", "user": "https://www.instagram.com/pabllovittar/"}],
+            "youtube": "https://www.youtube.com/channel/UCugD1HAP3INAiXo70S_sAFQ",
+            "spotify": "https://open.spotify.com/artist/6tzRZ39aZlNqlUzQlkuhDV?si=kwY83cKHQDyIAgdtTJnppw"
+        },
+        "referencias": ["https://www.deezer-blog.com/br/pabllo-vittar/"]
     },
 
     {
         "name": "Juliano Holanda",
-        "spotify_link": "https://open.spotify.com/artist/3wg2dojocgkzfZFx66oHSt?si=BLuK2yNLTFqPsyvJe8agiQ",
         "container-name": "juliano-holanda",
+        "image": "images/juliano-holanda.jpg",
+        "text": "Nascido em 1977 em Goiana, cidade da Zona da Mata de Pernambuco, Juliano Holanda começou a tocar violão aos nove anos de idade e, aos 13, subiu no palco pela primeira vez.Entre 2012 e 2021, Juliano Holanda - que já teve canções gravadas por Elba Ramalho e Zélia Duncan, além de atuar como multi-instrumentista, produtor e arranjador de diversos projetos musicais - lançou três álbuns solo, apresentando algumas das suas centenas de composições.",
+        "socialMedia": {
+            "instagram": [{"name": "Juliano Holanda", "user": "https://www.instagram.com/julianoholanda/"}],
+            "youtube": "https://www.youtube.com/channel/UC5teC17c-6LTsrGxnwykXdQ",
+            "spotify": "https://open.spotify.com/artist/3wg2dojocgkzfZFx66oHSt?si=BLuK2yNLTFqPsyvJe8agiQ"
+        },
+        "referencias": ["https://www12.senado.leg.br/radio/1/som-brasilis-1/2021/07/30/juliano-holanda-pernambuco"]
     },
 
     {
         "name": "Nelson Gonçalves",
-        "spotify_link": "https://open.spotify.com/artist/3wg2dojocgkzfZFx66oHSt?si=BLuK2yNLTFqPsyvJe8agiQ",
         "container-name": "nelson-goncalves",
+        "image": "images/nelson-gonçalves.jpg",
+        "text": "Antônio Gonçalves Sobral (Santana do Livramento, Rio Grande do Sul, 1919 - Rio de Janeiro, Rio de Janeiro, 1998). Cantor e compositor. Conhecido como Rei do Rádio, é considerado um dos cantores brasileiros mais populares dos anos 1950. Maior vendedor de discos dessa década, tem como principal marca musical a interpretação de canções românticas e melodramáticas, com uma voz grave e empostada.",
+        "socialMedia": {
+            "instagram": [],
+            "youtube": "https://www.youtube.com/channel/UCPr3AhjfVMVcVuSfA21Ml8Q",
+            "spotify": "https://open.spotify.com/artist/2wi99swSP87YaFjVOI3zGJ?si=2yCBqbE1S6S4uKIeeU3dYA"
+        },
+        "referencias": ["https://enciclopedia.itaucultural.org.br/pessoa359602/nelson-goncalves"]
+   
     },
 
     {
         "name": "Iza",
-        "spotify_link": "https://open.spotify.com/artist/3zgnrYIltMkgeejmvMCnes?si=XAAc7peGTQ2uAWvg8gLtHA",
         "container-name": "iza",
+        "image": "images/iza.jpg",
+        "text": "Isabela Cristina Corrêa de Lima, conhecida como IZA, é uma cantora brasileira. Nascida e criada no bairro de Olaria, Zona Norte do Rio de Janeiro, IZA começou a cantar no coral de uma igreja logo na infância e, aos 14 anos, começou a fazer pequenos shows em retiros e outras paróquias da região. É formada no curso de Publicidade e Propaganda e em 2016 foi descoberta pela Warner Music através de alguns covers publicados no YouTube.",
+        "socialMedia": {
+            "instagram": [{"name": "Iza", "user": "https://www.instagram.com/iza/"}],
+            "youtube": "https://www.youtube.com/channel/UCWmh948hhrAwDkOfkzNGAaQ",
+            "spotify": "https://open.spotify.com/artist/3zgnrYIltMkgeejmvMCnes?si=XAAc7peGTQ2uAWvg8gLtHA"
+        },
+        "referencias": ["https://www.purebreak.com.br/famosos/iza_e549886"]
+   
     },
 
     {
         "name": "Tim Maia",
-        "spotify_link": "https://open.spotify.com/artist/0jOs0wnXCu1bGGP7kh5uIu?si=A-jWMgl0QE-CNWttmeeQUA",
         "container-name": "tim-maia",
+        "image": "images/tim-maia.jpg",
+        "text": "Sebastão Rodrigues Maia nasceu no dia 28 de setembro de 1942, na Rua Afonso Pena 24, coração do tradicionalíssimo bairro da Tijuca, zona norte do Rio de Janeiro. Por volta dos sete anos, já compunha suas primeiras músicas. Em 1968, produziu o disco “A Onda É o Boogaloo”, de Eduardo Araújo. Pioneiro, Tim foi o grande responsável por trazer a sonoridade da soul music para a Jovem Guarda.",
+        "socialMedia": {
+            "instagram": [],
+            "youtube": "https://www.youtube.com/channel/UC2h2RtfhRdMhBNt_TjY6dmA",
+            "spotify": "https://open.spotify.com/artist/0jOs0wnXCu1bGGP7kh5uIu?si=A-jWMgl0QE-CNWttmeeQUA"
+        },
+        "referencias": ["http://www.timmaia.com.br/primeiros-anos/"]
     },
 
 
